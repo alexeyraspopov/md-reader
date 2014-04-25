d47877b8fad71c140ff64646143d4f8b6b089971
Avenger7x <avenger7x13@gmail.com>
2014-04-25 17:50:34 +0300

    styles

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var fs = require('fs'),
 	marked = require('marked'),
 	opn = require('opn'),
-	tempfile = require('tempfile');
+	tempfile = require('tempfile'),
+	path = require('path'),
+	stylesheet = '<link rel="stylesheet" href="' + path.join(process.cwd(), 'styles/github.css') + '">';
 
 module.exports = function(filename){
 	var content = fs.readFileSync(filename, 'utf-8'),
 		temp = tempfile('.html');
 
-	content = marked(content);
+	content = stylesheet + marked(content);
 	fs.writeFileSync(temp, content);
 	opn(temp);
 };
