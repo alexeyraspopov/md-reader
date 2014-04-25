443abd8c68976ca12883e8464656fe5971acbdd6
Avenger7x <avenger7x13@gmail.com>
2014-04-25 17:43:21 +0300

    parse markdown and open

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your awesome code here
+var fs = require('fs'),
+	marked = require('marked'),
+	opn = require('opn'),
+	tempfile = require('tempfile');
+
+module.exports = function(filename){
+	var content = fs.readFileSync(filename, 'utf-8'),
+		temp = tempfile('.html');
+
+	content = marked(content);
+	fs.writeFileSync(temp, content);
+	opn(temp);
+};
